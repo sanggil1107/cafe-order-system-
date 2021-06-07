1c124ce2ada77044e49b774605a28c315b9d2eb0
sanggil1107 <yangpang1107@gmail.com>
2021-06-07 23:45:29 +0900

    cafe order init

---
+++ src/main/java/cafeorder/CafeorderApplication.java
@@ -0,0 +1,13 @@
+package cafeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafeorder/CafeorderApplicationTests.java
@@ -0,0 +1,13 @@
+package cafeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
