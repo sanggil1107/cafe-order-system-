7adca8a76086e5912e1893a77d744a6ea9c25eb3
sanggil1107 <yangpang1107@gmail.com>
2021-06-13 23:20:23 +0900

    springboot react 연동

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{message}</h1>
+      </header>
+    </div>
+  )
+
+}
+
+
+export default App;
--- src/main/java/cafeorder/controller/OrderController.java
+++ src/main/java/cafeorder/controller/OrderController.java
@@ -1,5 +1,16 @@
 package cafeorder.controller;
 
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
 
+  @RequestMapping("/api/hello")
+  public String hello() {
+    return "hi. time is " + new Date() + "\n";
+  }
 }
