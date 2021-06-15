35f283d80406836e6ce34130a2c0a6c98a08dee1
sanggil1107 <yangpang1107@gmail.com>
2021-06-15 23:51:58 +0900

    리뷰 기능

--- src/main/java/cafeorder/controller/ReviewController.java
+++ src/main/java/cafeorder/controller/ReviewController.java
@@ -1,13 +1,20 @@
 package cafeorder.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import cafeorder.service.ReviewService;
+
 @RestController
 public class ReviewController {
   
+  @Autowired
+  private ReviewService reviewService;
+
   // 리뷰 목록
   @RequestMapping(value = "/review", method = RequestMethod.GET)
   public String ReviewList(Model model) throws Exception {
@@ -19,4 +26,28 @@ public class ReviewController {
   public String ReviewWrite() throws Exception {
     return "reviewwrite";
   }
+
+  // 리뷰 작성
+  @RequestMapping(value = "/review/write", method = RequestMethod.POST)
+  public String InsertReview() throws Exception {
+    return "redirect:/review";
+  }
+
+  // 리뷰 상세 화면
+  @RequestMapping(value = "/review/{reviewIdx}", method = RequestMethod.GET)
+  public String ReviewDetail(Model model) throws Exception {
+    return "reviewdetail";
+  }
+
+  // 리뷰 수정
+  @RequestMapping()
+  public String UpdateReview() throws Exception {
+    return "redirect:/review";
+  }
+  // 리뷰 삭제
+  @DeleteMapping(value = "/review/{reviewIdx}")
+  public String DeleteReview() throws Exception {
+    return "redirect:/review";
+  }
+
 }
