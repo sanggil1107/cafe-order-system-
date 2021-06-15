5373e762e6e044d27f7e8c6a20569aea5f6b4448
sanggil1107 <yangpang1107@gmail.com>
2021-06-16 00:35:01 +0900

    리뷰 기능 정의

--- src/main/java/cafeorder/service/ReviewService.java
+++ src/main/java/cafeorder/service/ReviewService.java
@@ -2,4 +2,9 @@ package cafeorder.service;
 
 public interface ReviewService {
   
+  void selectReviewList() throws Exception;
+  void saveReview() throws Exception;
+  void selectReviewDetail() throws Exception;
+  void deleteReview() throws Exception;
+  
 }
--- src/main/java/cafeorder/service/ReviewServiceImpl.java
+++ src/main/java/cafeorder/service/ReviewServiceImpl.java
@@ -1,5 +1,27 @@
 package cafeorder.service;
 
-public class ReviewServiceImpl {
-  
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+  @Override
+  public void selectReviewList() throws Exception {
+
+  }
+
+  @Override
+  public void saveReview() throws Exception {
+
+  }
+
+  @Override
+  public void selectReviewDetail() throws Exception {
+
+  }
+
+  @Override
+  public void deleteReview() throws Exception {
+    
+  }
 }
