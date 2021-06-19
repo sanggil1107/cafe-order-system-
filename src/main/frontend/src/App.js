806e9ccedbd620d1ca856d81e05ade16be630164
sanggil1107 <yangpang1107@gmail.com>
2021-06-19 15:55:36 +0900

    화면 개발

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,28 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import Review from './components/Review';
 
 const App = () => {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{message}</h1>
-      </header>
+    <div>
+      <Review></Review>
     </div>
-  )
+  );
+  // const [message, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   fetch('/api/hello')
+  //     .then(response => response.text())
+  //     .then(message => {
+  //       setMessage(message);
+  //     });
+  // },[])
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <h1 className="App-title">{message}</h1>
+  //     </header>
+  //   </div>
+  // )
 
 }
-
 
 export default App;
