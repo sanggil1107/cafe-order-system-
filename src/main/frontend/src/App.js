27cd5788a7f19bf9626107281cf1f9c211fbf4f3
sanggil1107 <yangpang1107@gmail.com>
2021-06-27 22:01:50 +0900

    화면 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,29 +4,29 @@ import Main from './components/Main';
 
 const App = () => {
 
-    return (
-        <div>
-            <Main></Main>
-        </div>
-    );
-    // const [message, setMessage] = useState("");
+  return (
+    <div>
+      <Main></Main>
+    </div>
+  );
+  // const [message, setMessage] = useState("");
 
-    // useEffect(() => {
-    //   fetch('/api/hello')
-    //     .then(response => response.text())
-    //     .then(message => {
-    //       setMessage(message);
-    //     });
-    // },[])
+  // useEffect(() => {
+  //   fetch('/api/hello')
+  //     .then(response => response.text())
+  //     .then(message => {
+  //       setMessage(message);
+  //     });
+  // },[])
 
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">{message}</h1>
-    //     </header>
-    //   </div>
-    // )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <h1 className="App-title">{message}</h1>
+  //     </header>
+  //   </div>
+  // )
 
 }
 
