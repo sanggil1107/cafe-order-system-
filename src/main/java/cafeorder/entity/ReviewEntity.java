d23c8b952ffcef4e8bfa15b8573dc13ffeefbc1e
sanggil1107 <yangpang1107@gmail.com>
2021-06-28 23:53:15 +0900

    jpa 적용

--- src/main/java/cafeorder/entity/ReviewEntity.java
+++ src/main/java/cafeorder/entity/ReviewEntity.java
@@ -2,20 +2,15 @@ package cafeorder.entity;
 
 import java.time.LocalDateTime;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class ReviewEntity {
 
-  private int reviewId;
-  private String title;
-  private String contents;
-  private int cnt;
-  private String userId;
-	private LocalDateTime createTime;
-	private String deletedYn;
-  private String cafeId;
+    private int reviewId;
+    private String title;
+    private String contents;
+    private int cnt;
+    private String userId;
+    private LocalDateTime createTime;
+    private String deletedYn;
+    private String cafeId;
 
 }
--- src/main/java/cafeorder/entity/UserEntity.java
+++ src/main/java/cafeorder/entity/UserEntity.java
@@ -1,16 +1,11 @@
 package cafeorder.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UserEntity {
-  
-  private String userId;
-  private String userName;
-  private String password;
-  private String tel;
-  private int points;
+
+    private String userId;
+    private String userName;
+    private String password;
+    private String tel;
+    private int points;
 
 }
