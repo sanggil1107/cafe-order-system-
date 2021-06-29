c5402d8bc2e1b54c44ce4baed38d3592ca731a91
sanggil1107 <yangpang1107@gmail.com>
2021-06-30 00:16:59 +0900

    controller 수정

--- src/main/java/cafeorder/controller/ReviewController.java
+++ src/main/java/cafeorder/controller/ReviewController.java
@@ -11,43 +11,49 @@ import cafeorder.service.ReviewService;
 
 @RestController
 public class ReviewController {
-  
-  @Autowired
-  private ReviewService reviewService;
 
-  // 리뷰 목록
-  @RequestMapping(value = "/review", method = RequestMethod.GET)
-  public String ReviewList(Model model) throws Exception {
-    return "review";
-  }
+    @Autowired
+    private ReviewService reviewService;
 
-  // 리뷰 작성 화면
-  @RequestMapping(value = "/review/write", method = RequestMethod.GET)
-  public String ReviewWrite() throws Exception {
-    return "reviewwrite";
-  }
+    // 리뷰 목록
+    @RequestMapping(value = "/review", method = RequestMethod.GET)
+    public String ReviewList(Model model) throws Exception {
+        reviewService.selectReviewList();
+        return "review";
+    }
 
-  // 리뷰 작성
-  @RequestMapping(value = "/review/write", method = RequestMethod.POST)
-  public String InsertReview() throws Exception {
-    return "redirect:/review";
-  }
+    // 리뷰 작성 화면
+    @RequestMapping(value = "/review/write", method = RequestMethod.GET)
+    public String ReviewWrite() throws Exception {
+        return "reviewwrite";
+    }
 
-  // 리뷰 상세 화면
-  @RequestMapping(value = "/review/{reviewIdx}", method = RequestMethod.GET)
-  public String ReviewDetail(Model model) throws Exception {
-    return "reviewdetail";
-  }
+    // 리뷰 작성
+    @RequestMapping(value = "/review/write", method = RequestMethod.POST)
+    public String InsertReview() throws Exception {
+        reviewService.saveReview();
+        return "redirect:/review";
+    }
 
-  // 리뷰 수정
-  @RequestMapping()
-  public String UpdateReview() throws Exception {
-    return "redirect:/review";
-  }
-  // 리뷰 삭제
-  @DeleteMapping(value = "/review/{reviewIdx}")
-  public String DeleteReview() throws Exception {
-    return "redirect:/review";
-  }
+    // 리뷰 상세 화면
+    @RequestMapping(value = "/review/{reviewIdx}", method = RequestMethod.GET)
+    public String ReviewDetail(Model model) throws Exception {
+        reviewService.selectReviewDetail();
+        return "reviewdetail";
+    }
+
+    // 리뷰 수정
+    @RequestMapping()
+    public String UpdateReview() throws Exception {
+        reviewService.saveReview();
+        return "redirect:/review";
+    }
+
+    // 리뷰 삭제
+    @DeleteMapping(value = "/review/{reviewIdx}")
+    public String DeleteReview() throws Exception {
+        reviewService.deleteReview();
+        return "redirect:/review";
+    }
 
 }
