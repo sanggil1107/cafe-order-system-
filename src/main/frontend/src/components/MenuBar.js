6d41e55923fb072bc626efba02bdd5e55881f74c
sanggil1107 <yangpang1107@gmail.com>
2021-07-06 23:50:56 +0900

    화면 추가

---
+++ src/main/frontend/src/components/MenuBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MenuBar = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default MenuBar;
---
+++ src/main/frontend/src/components/Product_Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Product_Menu = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Product_Menu;
