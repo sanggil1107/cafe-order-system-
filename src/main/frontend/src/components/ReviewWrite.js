5de47330feb18ec3f440d4cb1b62abe4240df77a
sanggil1107 <yangpang1107@gmail.com>
2021-07-07 23:56:36 +0900

    게시판 작성

--- src/main/frontend/src/components/ReviewWrite.js
+++ src/main/frontend/src/components/ReviewWrite.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import './review.css'
+import { useState } from 'react';
+import ReviewService from '../service/ReviewService';
 
 const ReviewWrite = (props) => {
+
+    const [review, setReview] = useState({
+        title: '',
+        content: ''
+    });
+
+    const getValue = e => {
+        const {name, value} = e.target;
+        console.log(name, value);
+        setReview({
+            ...review,
+            [name]: value,
+        });
+        console.log(review);
+    }
+
+    const onSubmit = () => {
+        ReviewService.insertReview(review).then(res => {
+            props.history.push("/review");
+        });
+    }
+
     return (
         <div class="container">
             <h2>게시판 등록</h2>
-            <form id="frm" name="frm" method="POST" action="/board/Insert" enctype="multipart/form-data">
+            <form id="frm" name="frm">
                 <table class="board_detail">
-                    <tr>
-                        <td>제목</td>
-                        <td><input type="text" id="title" name="title" /></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <textarea name="contents" id="contents"></textarea>
-                        </td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>제목</td>
+                            <td><input type="text" id="title" name="title" onChange={getValue}/></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <textarea name="content" id="content" onChange={getValue}></textarea>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
                 <input type="file" id="files" name="files" multiple="multiple" />
-                <input type="submit" id="submit" value="저장" class="btn" />
+                <input type="submit" id="submit" value="저장" class="btn" onClick={onSubmit} />
                 <input type="button" id="cancel" value="취소" class="btn" onClick={() => props.history.goBack()}/>
             </form>
         </div>
--- src/main/frontend/src/service/ReviewService.js
+++ src/main/frontend/src/service/ReviewService.js
@@ -5,6 +5,10 @@ const REVIEW_API_URL = "http://localhost:8080/review";
 class ReviewService {
     getReviews() {
         return axios.get(REVIEW_API_URL);
+    };
+
+    insertReview(review) {
+        return axios.post(REVIEW_API_URL + "/write", review);
     }
 }
 
