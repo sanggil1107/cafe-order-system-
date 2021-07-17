fdbbb32259723b8ff6cbc1c590856fd6e785e289
sanggil1107 <yangpang1107@gmail.com>
2021-07-17 23:58:11 +0900

    로그인 화면

--- src/main/frontend/src/components/Login.js
+++ src/main/frontend/src/components/Login.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChangeId = () => {
+
+    }
+
+    const onChangePw = () => {
+
+    }
+    
+    const onLogin = () => {
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <h1>Login</h1>
+
+            <form>
+                <div>
+                    <input type="text" onChange={onChangeId} required />
+                    <span></span>
+                    <label>ID</label>
+                    <input type="password" onChange={onChangePw} required />
+                    <span></span>
+                    <label>PW</label>
+                </div>
+                <input type="submit" value="Login" onClick={onLogin} />
+            </form>
+        </div>
     );
 }
 
