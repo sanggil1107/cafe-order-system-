7d612b1693a2359ad9d27713b5e694bf1a571b0f
sanggil1107 <yangpang1107@gmail.com>
2021-08-05 23:53:25 +0900

    제품 관련 기능

--- src/main/java/cafeorder/controller/ProductController.java
+++ src/main/java/cafeorder/controller/ProductController.java
@@ -1,5 +1,15 @@
 package cafeorder.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import cafeorder.service.ProductService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     
+    @Autowired
+    private ProductService productService;
 }
---
+++ src/main/java/cafeorder/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package cafeorder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cafeorder.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+    
+}
