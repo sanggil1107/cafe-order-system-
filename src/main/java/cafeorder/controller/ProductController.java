058e4c53f1c23b09efe3e7892ac99e3d1e7716e4
sanggil1107 <yangpang1107@gmail.com>
2021-08-06 23:54:48 +0900

    제품 controller

--- src/main/java/cafeorder/controller/ProductController.java
+++ src/main/java/cafeorder/controller/ProductController.java
@@ -1,15 +1,32 @@
 package cafeorder.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import cafeorder.entity.Product;
 import cafeorder.service.ProductService;
 
-@RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class ProductController {
     
     @Autowired
     private ProductService productService;
+
+    // 제품 조회
+    public List<Product> ProductList(Model model) throws Exception {
+        return null;
+    }
+
+
+    // 제품 상세정보
+    public Product ProductDetail(@RequestParam("productId") int productId) throws Exception {
+        return null;        
+    }
 }
